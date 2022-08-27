ff86394ca34d65970fe2c7df4488e5690d7f8734
naimetysz <nnaimetuysuz@gmail.com>
2022-08-27 17:00:43 +0300

    ders devami

--- html-css-11/app.js
+++ html-css-11/app.js
@@ -17,8 +17,10 @@
 // eger bu ozelliggi wrap yaparsak asagi iner ama tek satira sigmasini istersek no-wrap deriz
 // mesela biz soyle yapabiliriz ekran boyutuna ekrana soyle yazdir boyle yazdir diye bilriizi media -querylerle 
 // 3.flexbox:justfy-content hizalama yatayda hizalamak burada hizalamayi  birden fazla element icin yapabilirzi display;flex justif-content ; flex-start/flex-end/center/space-between/space-around
+
 // ile sola/saga/ortaya/sagdan soldan marginli/space-evenly(butun alanlari esit boler),space-around(sagdan soldan belli bosluk verir betwene gore biraz fazla)
 // 4. felxbox;align-items; flexbox icindeki elemenlari dikeyde hizalamak icin kullanilir
+
 // align-items;konteynirin belli yuksekligi var bu buyukkluk icinde flex-start ile yukari dogru/flex-end ile asagi dogru/center ortaya dogru/stech yukaridan asagiya dogru siralaar
 // siralar.ancak sterc kullanmak icin elemnetin sabit yukseligi olmamali icerdeki itemin sabit yukseligi olmaz /baseline elemneton yazilarina gore hizalar
 // flexbox;align -content cok satirli elemanlari siralamak icin kullanilir align-content hepsini tek bir paket gibi dusunup hepsini hizalar
@@ -27,4 +29,11 @@
 // 5.flexbox gap marginle arasini actik argin demek elemnetin disi ile ilgili bosluk demek gap de kucuk bir nuans var item arasi sutun ve satirlarin 
 // boslugunu belirliyor dis hacim vermek istemiyorsak gap verebiliriz ama bunu tasiyiciya vericez
 // yatay ve dikey bosluklari belirlemek icin  kullanilir ilk yazilan yatay satirlar 2.ise dikey sutunlar arasindaki bosluktur
-//
+// ara not .container>div{$}*9 degisken olusturp yaptim
+
+
+// flexbox(flex-grow)elementlerin buyume oranini belirler itemlara goreceli deger verdigimizde buyuyp kuculdgunde ayni deger kalmasini isteyebiliriz biri digerlerinden daha fazla yada daha az buyusun isteyebiliriz
+// flex-grow ozellihini digerlerine gore buyultebiliriz
+//flexbox(flex-shrink)digerlerine gore kuculsun istiyorsak kullaniyoruz 
+//flex-basis satirdaki maximum orani nasil paylastiracgimiz gosterir
+// auotu olarak kalirsa var olani verir 0 yaparsak herbir elemanin buyuklugu icerigi kadar olur 
