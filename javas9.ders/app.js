39e6618654de535023562c98a022c6bce2a84f28
naimetysz <nnaimetuysuz@gmail.com>
2022-08-30 09:10:23 +0300

    last change

--- javas9.ders/app.js
+++ javas9.ders/app.js
@@ -13,23 +13,23 @@
  //console.log(typeof isimler);
 // console.log(isimler.length);
 
-const bosDizi = [];
-// console.log(bosDizi);
+///const bosDizi = [];
+//console.log(bosDizi);
 
 // // ! 2.Yöntem (Object Constructor)
-// const diller = new Array("C++", "javascript", "Assembly", "Go", "Ruby");
+//const diller = new Array("C++", "javascript", //"Assembly", "Go", "Ruby");
 // console.log(diller);
 
-// const numbers = new Array(3, 2, 1);
-// console.log(numbers);
+ //const numbers = new Array(3, 2, 1);
+ //console.log(numbers);
 
 // //! 10 elemanlik bos bir Array oluşturdu
-// const numbers1 = new Array(10);
-// console.log(numbers1);
+ //const numbers1 = new Array(10);
+//console.log(numbers1);
 
 // // ! 3.Yöntem (Array.of())
-// const veriler = Array.of(1, 2, 3);
-// console.log(veriler);
+//const veriler = Array.of(10);
+ //console.log(veriler);
 
 // const veri = Array.of("Deneme");
 // console.log(veri);
@@ -37,35 +37,35 @@ const bosDizi = [];
 // //* Diziden Veri Okuma-Yazma (indisleme)
 // //* ---------------------------------------------------------
 // //!  Diziden veri okuma
-// console.log(diller[1]);
-// const go = diller[3];
+ //console.log(diller[1]);
+ //const go = diller[3];
 // console.log(go);
 
 // console.log(isimler[isimler.length - 1]);
 // //? at()
-// console.log(isimler.at(-1));
+//console.log(isimler.at(-1));
 
 // //!  Diziye veri yazma
-// // const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
-// isimler[1] = "saffet";
-// console.log(isimler);
 
-// //!Uncaught TypeError: Assignment to constant variable.
-// // isimler = ["Ali", "Veli"];
+//const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
+//isimler[1] = "saffet";
+ //console.log(isimler);
+
+//!Uncaught TypeError: Assignment to constant variable.
+// isimler = ["Ali", "Veli"];
 
 // //? numbers dizisinin son elementinin degerini bir arttirin
 // console.log(numbers);
 // numbers[numbers.length - 1]++;
 // console.log(numbers);
 
-// const yaslar = [22, 44, 55];
-// const kisiler = ["Ahmet", "Can", 2022, 2022 - 1990, true, yaslar];
-
-// console.log(kisiler);
+//const yaslar = [22, 44, 55];
+ //const kisiler = ["Ahmet", "Can", 2022, 2022 - //1990, true, yaslar];
+ //console.log(kisiler);
 
 // //? kisiler icerisindeki yaslar dizisinde bulunan 55 yasini konsola yazdiriniz.
 
-// const yas55 = kisiler[5][2];
+//const yas55 = kisiler[5][2];
 // console.log(yas55);
 
 // //? kisiler icerisindeki yaslar dizisinde bulunan 44 yasini bir eksiltiniz.
@@ -77,22 +77,22 @@ const bosDizi = [];
 // //?  DIZIYI DEGISTIREN (MUTATOR) METOTLAR
 // //? ===========================================================
 
-// const arabalar = ["BMW", "Mercedes", "Fiat", "Anadol"];
+ const arabalar = ["BMW", "Mercedes", "Fiat", "Anadol"];
 
-// //** pop() son elemanı siler ve sildigi elemani dondurur.
-// const silinen = arabalar.pop();
+//** pop() son elemanı siler ve sildigi elemani dondurur.
+//const silinen = arabalar.pop();
 // console.log(arabalar, silinen);
 
 // //* push() dizinin sonuna eleman ekler ve dizinin son eleman sayisini dondurur.
 
-// const n = arabalar.push("Citroen");
-// console.log(arabalar, n);
+ //const n = arabalar.push("Citroen");
+ //console.log(arabalar, n);
 
 // //* unshift dizinin 0. indeksine eleman ekler ve dizinin son eleman sayisini dondurur.
-// const n1 = arabalar.unshift("Audi");
-// console.log(arabalar, n1);
+ //const n1 = arabalar.unshift("Audi");
+//console.log(n1);
 
-// //* dizinin 0. indeks elemanini siler ve silenen elemani dondurur
+//shift //* dizinin 0. indeks elemanini siler ve silenen elemani dondurur
 // const audi = arabalar.shift();
 // console.log(arabalar, audi);
 
@@ -103,8 +103,8 @@ const bosDizi = [];
 // arabalar.splice(1, 0, "Passat");
 // console.log(arabalar);
 
-// arabalar.splice(3, 1, "Honda", "Toyota");
-// console.log(arabalar);
+//arabalar.splice(3, 1, "Honda", "Toyota");
+//console.log(arabalar);
 
 // //* Dizinin tamamini ters siraya cevirir.
 // arabalar.reverse();
@@ -124,20 +124,23 @@ const bosDizi = [];
 // //* ile buyuk sayilari yer degistirerek siralama islemini gerceklestirir.
 // sayilar.sort((a, b) => a - b);
 // console.log(sayilar);
+//* fill()
+const array1 = [1, 2, 3, 4];
+ //array1.fill(0);
+ //.log(array1)
+ // butun elemanlari o ile doldurur;
 
-// //* fill()
-// const array1 = [1, 2, 3, 4];
-// array1.fill(0);
-// console.log(array1);
-
-// array1.fill(1, 2, 4);
-// console.log(array1);
+ //array1.fill(1, 2, 4);
+ //console.log(array1);
 // array1.fill(-1, 1); //* 1. eleman ve sonrasini -1 yap
 // console.log(array1);
+// 1. parametre= diziye yazilacak deger
+// 2.parametre =baslangic indisi
+// 3.parametre =bitis indisi
 
 // //? ===========================================================
 // //?  DIZI ERISIM METOTLARI
-// //? ===========================================================
+// //? // bunlar diziyi degistirmez===========================================================
 // const sayilar1 = [3, 5, 2, "2", "uc", 2, "3", "bes", "5"];
 
 // //* includes()
@@ -148,7 +151,7 @@ const bosDizi = [];
 // //* indexOf(), lastIndexOf();
 // //*-----------------------------------------------------------
 // //* ilk eslesen indeksi dondurur.
-
+// 2.eleman 5. indekste
 // console.log(sayilar1.indexOf(2)); //? 2
 // console.log(sayilar1.lastIndexOf(2)); //? 5
 // console.log(sayilar1.lastIndexOf(4)); //? -1
@@ -174,9 +177,9 @@ const bosDizi = [];
 
 // //* slice()
 // //*-----------------------------------------------------------
-// const arabalar1 = ["BMW", "Mercedes", "Fiat", "Anadol"];
+ //const arabalar1 = ["BMW", "Mercedes", "Fiat", "Anadol"];
 // const yeniArabalar = arabalar1.slice(2);
-// console.log(yeniArabalar, arabalar1);
+//console.log(yeniArabalar, arabalar1);//2. idexten basla sona kadar git
 
 // const yeniArabalar1 = arabalar1.slice(1, 3);
 // console.log(yeniArabalar1);
