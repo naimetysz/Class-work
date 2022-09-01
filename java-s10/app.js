6312f8b366437ee0871e96363328e31a70ac1512
naimetysz <nnaimetuysuz@gmail.com>
2022-09-01 16:28:49 +0300

    soru 1 eklendi

---
+++ java-s10/app.js
@@ -0,0 +1,19 @@
+const negatifler = [];
+const pozitifler = [];
+
+const dizelereAyir = (n) => {
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] < 0) {
+      negatifler.push(n[i]);
+    } else {
+      pozitifler.push(n[i]);
+    }
+  }
+};
+dizelereAyir(dizi);
+console.log(negatifler);
+console.log(pozitifler);
+console.log(dizi);
+
+
+    
