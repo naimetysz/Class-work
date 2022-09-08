7711dadda2563ca610d82ce1b8b51a2f6d000f15
naimetysz <nnaimetuysuz@gmail.com>
2022-09-08 17:25:48 +0300

    change

---
+++ sass/app.js
@@ -0,0 +1,6 @@
+//// SASS DERSI //
+// SASS VE SCSS arasindaki fark su sass da ruby dili ile yazidligi icin suslu vs yok ama genelde insasnlar scss kullaniyorlar syntaxdan dolayi
+// suslu ve noktali virgul oldugu icin node js enviromenti scss i css e ceviriri
+// node js browserda calisan javascript motorunun locale inmis hali java s uygulamalarini browser olmadam kulalnabiiriz
+//sass ve scss icice yapiyi destekler sass yazip klasik moda cevrildi
+// derleyici durdurursak degisiklikler gelmez artik
