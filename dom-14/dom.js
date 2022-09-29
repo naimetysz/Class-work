4c19470e5949453ea0ea8630933311df5a46e072
naimetysz <nnaimetuysuz@gmail.com>
2022-09-29 10:07:04 +0300

    change

--- dom-14/dom.js
+++ dom-14/dom.js
@@ -2,3 +2,10 @@ const h1 = document.getElementById("header");
 console.log(h1);
 h1.style.background ="red";
 
+const list = document.getElementsByTagName("li");
+list[2].style.color ="red"
+
+const lists = document.querySelectorAll("ul li");
+console.log(lists);
+
+
