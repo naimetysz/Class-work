a18b1d8686f2be9cca7053d6fd59ead3cdb0da6f
naimetysz <nnaimetuysuz@gmail.com>
2022-09-29 16:08:12 +0300

    change

--- events-dom/2-example/app.js
+++ events-dom/2-example/app.js
@@ -5,13 +5,3 @@ const langList = document.getElementById("#lang-list");
 const newUl = document.createElement("ul")
 langList.appendChild(newUL);
 
-addBtn.addEventListener('click'() => {
-    if(!langInput.value) {
-        alert("please enter a language");
-
-    } else {
-        newUl.innerHTML += `<li>${langInput.value}</li>`;
-        langInput.value = "";
-        
-    }
-});
