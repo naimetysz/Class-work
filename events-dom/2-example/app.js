cc380a9fbae891d830dffc4eba4f4d85f41cef43
naimetysz <nnaimetuysuz@gmail.com>
2022-09-29 16:11:05 +0300

    chanegr

--- events-dom/2-example/app.js
+++ events-dom/2-example/app.js
@@ -1,7 +1,9 @@
 const langInput = document.querySelector(".lang-input");
+console.log(langInput)
 const addBtn = document.querySelector("#add-btn");
 const deleteBtn =document.querySelector("#delete-btn");
 const langList = document.getElementById("#lang-list");
-const newUl = document.createElement("ul")
-langList.appendChild(newUL);
+const newUl = document.createElement("ul");
+langList.appendChild(newUl);
+
 
