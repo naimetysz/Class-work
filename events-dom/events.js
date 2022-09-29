b729f657b8e3eb7e4058377621d9930b432a8dc8
naimetysz <nnaimetuysuz@gmail.com>
2022-09-29 12:54:37 +0300

    change

--- events-dom/events.js
+++ events-dom/events.js
@@ -10,3 +10,26 @@ h1.onmouseout = () => {
 h1.addEventListener("click",()=>{
     alert("mutlu ol:)")
 })
+document.getElementById("btn").addEventListener("click",function(){
+    const input =document.querySelector("#input")
+    if (input.value ===null){
+        alert ("degeri bos bebegim");
+    }else{
+        alert(`${input.value} girdin `)
+    }
+})
+///// macbook gibi buyume ornegi
+const list =document.querySelectorAll(".list")
+list.forEach((li)=>{
+    li.style.transition = "font-size 0.5s"
+    li.style.transform = "translateX(10px)"
+    li.style.lineHeigth = "2rem"
+    console.log(li);
+    li.onmouseover =()=>{
+        li.style.fontSize = "4rem";
+
+    };
+    li.onmouseout = ()=> {
+        li.style.fontSize = "1rem";
+    };
+})
