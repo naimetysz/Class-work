308e7b0691cdbb6be264f70cc04235d9b29530f4
naimetysz <nnaimetuysuz@gmail.com>
2022-09-30 11:55:20 +0300

    chaneg

--- guess-game/game.js
+++ guess-game/game.js
@@ -1,3 +1,10 @@
 //// Guess my number///
 const randomNumber = Math.floor(Math.random()*100);
 console.log(randomNumber)
+document.querySelector(".check-btn").addEventListener("click",()=>{
+    const guessInput = Number(document.querySelector(".guess-input").value);
+    console.log(guessInput);
+    if(!guessInput){
+        document.querySelector(".msg").innerText = "please enter a number";
+    }
+})
